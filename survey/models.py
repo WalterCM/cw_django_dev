aa566934c12411f0258d03981246edfdc62a50e5
Walter Capa <walterc316@gmail.com>
2023-12-02 15:45:11 -0500

    feat: Updated Question and Answer models. Added Vote model and basic creation tests

--- survey/models.py
+++ survey/models.py
@@ -1,18 +1,57 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.db import IntegrityError
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class QuestionManager(models.Manager):
+    def create_question(self, author=None, title=None, **kwargs):
+        if not author:
+            raise IntegrityError('Question requires an author')
+        if not title:
+            raise IntegrityError('Question requires a title')
+        question = self.model(author=author, title=title, **kwargs)
+        question.save()
+
+        return question
 
 
 class Question(models.Model):
     created = models.DateField('Creada', auto_now_add=True)
-    author = models.ForeignKey(get_user_model(), related_name='questions', verbose_name='Pregunta',
-                               on_delete=models.CASCADE)
-    title = models.CharField('Título', max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        related_name='questions',
+        verbose_name='Pregunta',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField('Título', max_length=200, blank=False, null=False)
     description = models.TextField('Descripción')
-    # TODO: Quisieramos tener un ranking de la pregunta, con likes y dislikes dados por los usuarios.
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
 
     def get_absolute_url(self):
         return reverse('survey:question-edit', args=[self.pk])
+
+
+class AnswerManager(models.Manager):
+    def create_answer(self, question=None, author=None, value=None, **kwargs):
+        if not question:
+            raise IntegrityError('Answer requires a question')
+        if not author:
+            raise IntegrityError('Answer requires an author')
+        if not value:
+            raise IntegrityError('Answer requires a value')
+
+        answer = self.model(question=question, author=author, value=value, **kwargs)
+        answer.save()
+
+        return answer
 
 
 class Answer(models.Model):
@@ -24,6 +63,24 @@ class Answer(models.Model):
                       (5, 'Muy Alto'),)
 
     question = models.ForeignKey(Question, related_name='answers', verbose_name='Pregunta', on_delete=models.CASCADE)
-    author = models.ForeignKey(get_user_model(), related_name='answers', verbose_name='Autor', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='answers', verbose_name='Autor', on_delete=models.CASCADE)
     value = models.PositiveIntegerField('Respuesta', default=0)
     comment = models.TextField('Comentario', default='', blank=True)
+
+    objects = AnswerManager()
+
+    def __str__(self):
+        return '{question} - {author}:{value}'.format(question=self.question, author=self.author, value=self.value)
+
+
+class Vote(models.Model):
+    question = models.ForeignKey(Question, related_name='votes', verbose_name='Pregunta', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='votes', verbose_name='Autor', on_delete=models.CASCADE)
+    is_like = models.BooleanField(null=False)
+
+    def __str__(self):
+        return '{question} - {author}:{like}'.format(
+            question=self.question,
+            author=self.author,
+            like='like' if self.is_like else 'dislike'
+        )
--- survey/tests.py
+++ survey/tests.py
@@ -1,3 +1,109 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from django.contrib.auth.models import User
+from survey.models import Question, Answer, Vote
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', password='12345')
+        self.question = Question.objects.create(
+            title='Que te parecio el ejercicio?',
+            description='Esta es una pregunta sobre la prueba tecnica',
+            author=self.user
+        )
+        self.new_question_data = {
+            'title': 'Te gusta el futbol?',
+            'description': 'Esta es una pregunta sobre futbol',
+            'author': self.user
+        }
+        self.new_answer_data = {
+            'question': self.question,
+            'author': self.user,
+            'value': 5,
+            'comment': 'El ejercicio me parecio muy bueno'
+        }
+
+        self.new_vote_data = {
+            'question': self.question,
+            'author': self.user,
+            'is_like': True
+        }
+
+    def test_question_creation(self):
+        """Tests the creation of a question with expected data"""
+        question = Question.objects.create_question(**self.new_question_data)
+        self.assertEqual(question.title, self.new_question_data.get('title'))
+        self.assertEqual(question.description, self.new_question_data.get('description'))
+        self.assertEqual(question.author, self.new_question_data.get('author'))
+
+    def test_question_creation_no_title(self):
+        """Tests that the function create_question won't create a question without title"""
+        question_data = self.new_question_data.copy()
+        del question_data['title']
+        with self.assertRaises(IntegrityError):
+            Question.objects.create_question(**question_data)
+
+    def test_question_creation_no_author(self):
+        """Tests that the function create_question won't create a question without an author"""
+        question_data = self.new_question_data.copy()
+        del question_data['author']
+        with self.assertRaises(IntegrityError):
+            Question.objects.create_question(**question_data)
+
+    def test_answer_creation(self):
+        """Tests the creation of an answer with expected data"""
+        answer = Answer.objects.create(**self.new_answer_data)
+        self.assertEqual(answer.value, self.new_answer_data.get('value'))
+        self.assertEqual(answer.question, self.new_answer_data.get('question'))
+        self.assertEqual(answer.author, self.new_answer_data.get('author'))
+        self.assertEqual(answer.comment, self.new_answer_data.get('comment'))
+
+    def test_answer_creation_no_value(self):
+        """Tests that the function create_answer won't create an answer without a value"""
+        answer_data = self.new_answer_data.copy()
+        del answer_data['value']
+        with self.assertRaises(IntegrityError):
+            Answer.objects.create_answer(**answer_data)
+
+    def test_answer_creation_no_question(self):
+        """Tests that the function create_answer won't create an answer without a question"""
+        answer_data = self.new_answer_data.copy()
+        del answer_data['question']
+        with self.assertRaises(IntegrityError):
+            Answer.objects.create_answer(**answer_data)
+
+    def test_answer_creation_no_author(self):
+        """Tests that the function create_answer won't create an answer without an author"""
+        answer_data = self.new_answer_data.copy()
+        del answer_data['author']
+        with self.assertRaises(IntegrityError):
+            Answer.objects.create_answer(**answer_data)
+
+    def test_vote_creation(self):
+        """Tests the creation of a vote with expected data"""
+        vote = Vote.objects.create(**self.new_vote_data)
+        self.assertEqual(vote.question, self.new_vote_data.get('question'))
+        self.assertEqual(vote.author, self.new_vote_data.get('author'))
+        self.assertEqual(vote.is_like, self.new_vote_data.get('is_like'))
+
+    def test_vote_no_question(self):
+        """Tests that the function create_vote won't create a vote without a question"""
+        vote_data = self.new_vote_data.copy()
+        del vote_data['question']
+        with self.assertRaises(IntegrityError):
+            Vote.objects.create(**vote_data)
+
+    def test_vote_no_author(self):
+        """Tests that the function create_vote won't create a vote without an author"""
+        vote_data = self.new_vote_data.copy()
+        del vote_data['author']
+        with self.assertRaises(IntegrityError):
+            Vote.objects.create(**vote_data)
+
+    def test_vote_no_like_or_dislike(self):
+        """Tests that the function create_vote won't create a vote without indicating like or dislike"""
+        vote_data = self.new_vote_data.copy()
+        del vote_data['is_like']
+        with self.assertRaises(IntegrityError):
+            Vote.objects.create(**vote_data)
